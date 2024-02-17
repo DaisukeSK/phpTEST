df2d56898293d13f62bb58bed2e71964657e679b
Daisuke <skd.itkn@gmail.com>
2024-02-16 23:15:10 -0800

    commit

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@
 <body>
     <h2>php TEST</h2>
 
-    <form action="./test.php" method="post">
+    <form action="./test" method="post">
         <input type="text" name="username" required>
         <input type="submit">
     </form>

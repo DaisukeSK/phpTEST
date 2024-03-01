eb15359c78a035f3fcfdaecca3ff92d52704f4f9
Daisuke <skd.itkn@gmail.com>
2024-03-01 13:30:50 -0800

    test

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@
 <body>
     <h2>php TEST</h2>
 
-    <form action="sample.html" method="post">
+    <form action="/sample.html" method="post">
         <input type="text" name="username" required>
         <input type="submit">
     </form>

887ba7557fb86d4df0cc697cb3d9e1e432385380
Daisuke <skd.itkn@gmail.com>
2024-03-01 13:36:05 -0800

    test

--- api/index.php
+++ api/index.php
@@ -12,7 +12,7 @@
         <input type="submit">
     </form>
 
-    <form action="../api/test2.php" method="post">
+    <form action="/test2" method="post">
         <input type="text" name="username" required>
         <input type="submit">
     </form>
